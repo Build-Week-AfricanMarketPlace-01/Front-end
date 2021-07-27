8b95a5829535c750e2cadc9f6e72da4b4bb74a63
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-27 15:25:16 -0700

    gave some styles to signup page and also the error message for missing fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import Login from './components/Login'
 import AddItems from './components/AddItems'
+import Signup from './components/Signup'
 function App() {
   return (
     <div className="App">
+      
       <AddItems/>
+      <Signup />
    <Login/>
     </div>
   );
