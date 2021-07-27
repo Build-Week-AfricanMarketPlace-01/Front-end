f2627ae55b7b85b6a559214d5efde167b8aa4f73
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-27 14:12:08 -0700

    styled add items page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import Login from './components/Login'
+import AddItems from './components/AddItems'
 function App() {
   return (
     <div className="App">
+      <AddItems/>
    <Login/>
     </div>
   );
--- frontend/src/components/AddItems.js
+++ frontend/src/components/AddItems.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import * as yup from 'yup'
 import {reach} from 'yup'
+import styled from "styled-components";
 
 const initialFormValues={
     name:'',
@@ -77,17 +78,21 @@ export default function AddItems(){
     }, [schema, formValues])
 
     return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <h1>Add an Item</h1>
-                <input 
+        
+            <FormContainer >
+                <H1>ADD AN ITEM</H1>
+                <form onSubmit={onSubmit}>
+                <InputContainer>
+                <Errors>{errors.name}</Errors>
+                <Input 
                 type='text' 
                 name='name' 
                 placeholder='Name'
                 onChange={onChange}
                 value={formValues.name}
                 />
-                <select name='place' onChange={onChange} value={formValues.place}>
+                <Errors>{errors.place}</Errors>
+                <Select name='place' onChange={onChange} value={formValues.place}>
                     <option value=''>--Select Location--</option>
                     <option value='Busia'>Busia</option>
                     <option value='Malaba'>Malaba</option>
@@ -97,17 +102,20 @@ export default function AddItems(){
                     <option value='Mutukula'>Mutukula</option>
                     <option value='Namanga'>Namanga</option>
                     <option value='Sirare'>Sirare</option>
-                </select>
+                </Select>
 
-                <input
+                <Errors>{errors.price}</Errors>
+                <Input
                 type='number'
                 name='price'
                 placeholder='Price'
                 onChange={onChange}
                 value={formValues.price}
+                min='1'
                 />
 
-                <input
+                <Errors>{errors.description}</Errors>
+                <Input
                 type='text'
                 name='description'
                 placeholder='Description'
@@ -115,16 +123,81 @@ export default function AddItems(){
                 value={formValues.description}
                 />
 
-                <input
+                <Errors>{errors.category}</Errors>
+                <Input
                 type='text'
                 name='category'
                 placeholder='Category'
                 onChange={onChange}
                 value={formValues.category}
                 />
-
-                <button disabled={disabled}>Add Item</button>
-            </form>
-        </div>
+            </InputContainer>
+                <Button disabled={disabled}>Add Item</Button>
+                </form>
+            </FormContainer>
+        
     )
 }
+
+const H1=styled.h1`
+font-size: 2em;
+`
+const FormContainer=styled.div`
+border:1px solid darkgrey;
+border-radius: 7px;
+margin:3rem 16rem;
+padding:2rem;
+text-align: center;
+`
+
+const InputContainer=styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin:4rem;
+`
+
+const Input=styled.input`
+border: 2px solid darkgrey;
+border-radius: 4px;
+font-size: 1.5em;
+margin:1rem 4rem;
+padding:14px;
+`
+
+const Select=styled.select`
+border: 2px solid darkgrey;
+border-radius: 4px;
+font-size: 1.5em;
+margin:1rem 4rem;
+padding:14px;
+`
+
+const Button = styled.button`
+  background-color: #ed4933;
+  border: 0;
+  border-radius: 5px;
+  color: white;
+  height: 3rem;
+  letter-spacing: 0.175em;
+  line-height: 3rem;
+  margin: 1rem auto;
+  padding: 0 2rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  width: 30%;
+  &:disabled {
+    color: black;
+    background-color: white;
+    border: 2px solid black;
+    cursor: not-allowed;
+  }
+  &:hover {
+    opacity: 0.8;
+    transition: all 0.5s ease-out;
+  }
+`
+
+const Errors=styled.div`
+color:red;
+`
